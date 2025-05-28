14be112f402e93df205a40d21691b5eede77d3a2
jonveras <jonathan.veras@live.com>
2025-05-28 09:52:52 -0300

    s

---
+++ Banco_SQL_Server/Analises/AnaliseAmbiente/GuiaTuning.sql
@@ -0,0 +1,10 @@
+----------------------------------------------------------------------------------
+
+--Rodar a query com o stastics on para ver qual tabela é a maior ofensora 
+
+set statistics io on
+
+--roda a query
+
+----------------------------------------------------------------------------------
+
