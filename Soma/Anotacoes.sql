b2306d5eb9df30fa8bfd2d0957fd2495f1a30724
Jonathan <jonathan.veras@live.com>
2025-06-17 15:45:30 -0300

    s

--- Soma/Anotacoes.sql
+++ Soma/Anotacoes.sql
@@ -1,3 +1,5 @@
 -- se parar o job SPED(000_0110_MIT_PROCESSAR_LF_ERROS_MIT_PROCESSAR_SPED), tem que rodar o ultimo step
 -- geralmente quem locka o banco é o job lx_processos
 -- se a fila de restauração do AG tiver alta, verificar o 9.100 e derrubar quem tiver fudendo o SA
+-- sempre q for mexer em algum job que já existe, alterar um codigo que esteja dentro, salva o codigo antigo ai\\192.168.9.91\File Server AAF\Departamentos\TI\SuporteDB\Documentacao\Jobs
+ 
