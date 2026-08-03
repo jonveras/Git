772952d11ea84702c27923439c8b88cac58c1f80
Jonathan <jonathan.veras@live.com>
2026-08-03 10:39:00 -0300

    S

--- Soma/desabilitar_tabelas_CDC.sql
+++ Soma/desabilitar_tabelas_CDC.sql
@@ -4,13 +4,13 @@ SELECT
     'EXEC sys.sp_cdc_disable_table
     @source_schema = N''dbo'',
     @source_name = N''' + [name] + ''',
-    @capture_instance = N''' + [name] + ''';' AS Script_Desabilitar,
+    @capture_instance = N''DBO_' + [name] + ''';' AS Script_Desabilitar,
 
     'EXEC sys.sp_cdc_enable_table
     @source_schema = N''dbo'',
     @source_name = N''' + [name] + ''',
     @role_name = NULL;' AS Script_Habilitar
-
+INTO
+	ANALISETI.DBO.CDC_TABELAS
 FROM sys.tables
 WHERE is_tracked_by_cdc = 1
-ORDER BY [name];
